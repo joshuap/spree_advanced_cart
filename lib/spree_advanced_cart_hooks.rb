68decd9b4781923704c6198346e01819cf22e85d
Joshua Wood <josh@hintmedia.com>
2011-05-13 08:25:40 -0700

    modified hook for javascript to accommodate javascript that is outside the document head.

--- lib/spree_advanced_cart_hooks.rb
+++ lib/spree_advanced_cart_hooks.rb
@@ -1,5 +1,5 @@
 class SpreeAdvancedCartHooks < Spree::ThemeSupport::HookListener
-  insert_after :inside_head do
+  insert_after :javascript do
     "<%= stylesheet_link_tag 'advanced_cart' %><%= javascript_include_tag 'advanced_cart' %>"
   end
 end
